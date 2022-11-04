ff0081cfb6bbf498c85f9c21ff126a8caa961a01
Haf <haf@protonmail.ch>
2022-11-04 22:24:07 +0100

    set up flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, ...}:
+  outputs = { self, rust-overlay, nixpkgs, ...}:
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs { 
+      inherit system;
+      overlays = [ (import rust-overlay) ];
+    };
   in with pkgs;
   {
     devShell.${system} = pkgs.mkShell {
       buildInputs = [
+        openssl
+        pkg-config
+        (rust-bin.beta.latest.default.override {
+          extensions = [ "rust-src" ];
+        })
         cargo
+        rust-analyzer
       ];
     }; 
   };
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+use std::cmp;
+use std::io;
+
 fn main() {
     println!("Hello, world!");
 }
