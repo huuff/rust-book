cecb75a3f255291dc646c56699af02e02f610db6
Haf <haf@protonmail.ch>
2022-11-06 10:44:21 +0100

    added some almost impossible levels

--- guessing_game/src/levels.rs
+++ guessing_game/src/levels.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 
 // TODO: Try to not make it a global. Create it in the main and pass it down
-pub static LEVELS: [Level; 5] = [
+pub static LEVELS: [Level; 10] = [
     Level {
         max_number: 10,
         max_tries: 10,
@@ -22,6 +22,26 @@ pub static LEVELS: [Level; 5] = [
         max_number: 50,
         max_tries: 6,
     },
+    Level {
+        max_number: 60,
+        max_tries: 5,
+    },
+    Level { 
+        max_number: 70,
+        max_tries: 4,
+    },
+    Level { 
+        max_number: 80,
+        max_tries: 3,
+    },
+    Level {
+        max_number: 90,
+        max_tries: 2,
+    },
+    Level {
+        max_number: 100,
+        max_tries: 1,
+    }
 ];
 
 pub struct Level {
